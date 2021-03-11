aa84913859ee998ed95565a32898bad4f1f769d1
mikietechie <mzinyoni7@outlook.com>
2021-03-11 09:08:30 +0200

    static files

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -3,15 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import NavbarComponent from './components/navbar-component/navbar-component';
 
-
+/*
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <NavbarComponent />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<NavbarComponent />
+			</div>
+		)
+	}
+}*/
+const App = () => {
+	return (
+		<div className="App">
+			<NavbarComponent />
+		</div>
+	)
 }
 
 export default App;
