ec868905ea9598730258e0febe0be206df5a91d5
mikietechie <mzinyoni7@outlook.com>
2021-03-15 08:49:30 +0200

    errors

--- clientside/src/components/customers-module/add-customer-component/edit-customer-component.js
+++ clientside/src/components/customers-module/add-customer-component/edit-customer-component.js
@@ -24,6 +24,19 @@ class EditCustomerComponent extends React.Component {
             address: "",
         })
     }
+    //  Having problems with this approach to selecting and updating customers. The following is an ugly but effective work around ...
+    initialiseForm = () => {
+        document.querySelector('#editCustomerName').value = this.props.customer.name;
+        document.querySelector('#editCustomerEmail').value = this.props.customer.email;
+        document.querySelector('#editCustomerPhone').value = this.props.customer.phone;
+        document.querySelector('#editCustomerAddress').value = this.props.customer.address;
+        
+    }
+    componentDidMount() {
+        this.initialiseForm()
+    }
+
+
     handleInputChange = (event) => {
         const {name, value} = event.target
         this.setState({
@@ -55,30 +68,30 @@ class EditCustomerComponent extends React.Component {
         return(
             <React.Fragment>
                  <div className="container">
-                    <form onSubmit={this.handleFormSubmit}>
+                    <form onSubmit={this.handleFormSubmit} id="editCustomerForm" name="editCustomerForm">
                         <legend>Update {this.props.customer.name}</legend>
                         <div className="form-group row">
                             <label className="col-sm-6 col-form-label">Name</label>
                             <div className="col-sm-6">
-                                <input type="text" name="name" className="form-control" placeholder="Mike Micheals" value={this.state.name} onChange={this.handleInputChange} />
+                                <input type="text" name="name" id="editCustomerName" className="form-control" placeholder="Mike Micheals" value={this.state.name} onChange={this.handleInputChange} />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-6 col-form-label">Email</label>
                             <div className="col-sm-6">
-                                <input type="text" name="email" className="form-control" placeholder="mm@mail.com" value={this.state.email} onChange={this.handleInputChange} />
+                                <input type="text" name="email" id="editCustomerEmail" className="form-control" placeholder="mm@mail.com" value={this.state.email} onChange={this.handleInputChange} />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-6 col-form-label">Phone</label>
                             <div className="col-sm-6">
-                                <input type="text" name="phone" className="form-control" placeholder="+263771588144" value={this.state.phone} onChange={this.handleInputChange} />
+                                <input type="text" name="phone" id="editCustomerPhone" className="form-control" placeholder="+263771588144" value={this.state.phone} onChange={this.handleInputChange} />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-6 col-form-label">Address</label>
                             <div className="col-sm-6">
-                                <textarea name="address" className="form-control" placeholder="Stand # 11 Lorraine Drive, Bluffhill, Harare" value={this.state.address} onChange={this.handleInputChange}></textarea>
+                                <textarea name="address" id="editCustomerAddress" className="form-control" placeholder="Stand # 11 Lorraine Drive, Bluffhill, Harare" value={this.state.address} onChange={this.handleInputChange}></textarea>
                             </div>
                         </div>
                         <div className="form-group row">
@@ -90,7 +103,6 @@ class EditCustomerComponent extends React.Component {
                     </form>
                 </div>
             </React.Fragment>
-
         )
     }
 }
